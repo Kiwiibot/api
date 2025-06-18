3868f49956f7dcdd49490cd8b46bb13994ba53b2
Lexedia <contact@lexedia.moe>
2025-06-19 00:14:42 +0200

    fuck my fat chungus life

--- images/src/tools/images/modification.rs
+++ images/src/tools/images/modification.rs
@@ -215,14 +215,14 @@ pub fn gif_reverse(image: Vec<u8>) -> Result<Vec<u8>, Error> {
 pub fn gif(image: Vec<u8>) -> Result<Vec<u8>, Error> {
     let mut codec = decode_image(image)?;
     let count = codec.get_frame_count();
-    let dur = codec.get_avg_duration();
+    let dur = codec.get_avg_duration()?;
 
     make_gif_or_combined_gif(
         vec![input_image(image)?],
-        |_: usize, imgs: Image| Ok(imgs[0]),
+        |_: usize, imgs: Vec<Image>| Ok(imgs[0].clone()),
         GifInfo {
             duration: dur,
-            frame_num: count,
+            frame_num: count as u32,
         },
         FrameAlign::NoExtend,
     )
